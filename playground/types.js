5dfec057b4888bc91b2a9fccc728006770626840
Ryan Bennek <rbennek.rb@gmail.com>
2022-04-17 17:27:44 -0500

    'update'

---
+++ playground/types.js
@@ -0,0 +1,21 @@
+/*eslint-disable */
+console.log('we in here');
+
+const name = 'ryan';
+const middle = "andrew";
+const last = `bennek`;
+
+const sentence = `she's so "cool"`;
+
+console.log(sentence);
+
+
+const hello = `Hello my name is ${name}. Nice to meet you. I am ${1+100} years old.`
+
+const html = `
+<div> 
+    <h2>${name}</h2>
+    <p>${hello}</p>
+</div>
+`
+

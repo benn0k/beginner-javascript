10ec9b115c8187e085c62f23e53bf038d8a41813
Ryan Bennek <rbennek.rb@gmail.com>
2022-05-02 19:26:48 -0500

    Update Dom Selectors

---
+++ exercises/20 - The DOM/the-dom.js
@@ -0,0 +1,9 @@
+console.log(`we in here!!!`);
+
+const p = document.querySelector('p');
+const img = document.querySelectorAll('.items img');
+const item2 = document.querySelector('.item2');
+const item2Image = item2.querySelector('img');
+const heading = document.querySelector('h2');
+heading.textContent = 'Ryan is Cool';
+console.dir(heading.textContent);

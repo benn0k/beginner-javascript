7a797858970a3e6e2259c1f104bc0cf8b1948c41
benn0k <rbennek.rb@gmail.com>
2022-09-07 21:52:07 -0500

    Update Etch-a-Sketch.js

--- exercises/33 - Etch-a-Sketch/etch-a-sketch.js
+++ exercises/33 - Etch-a-Sketch/etch-a-sketch.js
@@ -4,23 +4,24 @@ console.log(`we in here!!!`);
 const canvas = document.querySelector('#etch-a-sketch');
 const ctx = canvas.getContext('2d');
 const shakeButton = document.querySelector('.shake');
-const MOVE_AMOUNT = 10;
+const MOVE_AMOUNT = 20;
+
+//Create variables for width/height of canvas
+let canvasX = canvas.width;
+let canvasY = canvas.height;
 
 // Set up Canvas for drawing
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 10;
-// This starts the Draw
+ctx.lineWidth = 40;
 
 // set random num variables to use for starting point
 const ranNumber = (num) => Math.floor(Math.random() * num);
 
-// Not needed, but we can assign variables in objects like this. This creates two mutable variables named ranWidth and ranHeight = the width/height of the canvas.
-let { y, x } = canvas;
+
 
 x = ranNumber(1600);
 y = ranNumber(1000);
-
 
 // set beginning draw point
 ctx.beginPath();
@@ -28,29 +29,61 @@ ctx.moveTo(x, y);
 ctx.lineTo(x, y);
 ctx.stroke();
 
+// set initial hue color for line and set strokestyle color
+let hue = 0;
+ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+
 // Write Draw function
-function draw({key}){
-console.log(key)
-//start draw patch
-ctx.beginPath();
-ctx.moveTo(x,y);
-//Move to new location
-x -= MOVE_AMOUNT;
-y -= MOVE_AMOUNT;
-// Draw
-ctx.lineTo(x,y);
-ctx.stroke();
+function draw({ key }) {
+  console.log(key);
+  // Increment hue
+  hue += 1;
+  ctx.strokeStyle = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  //start draw path
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  //Move to new location
+  switch (key) {
+    case 'ArrowUp':
+      y -= MOVE_AMOUNT;
+      break;
+    case 'ArrowDown':
+      y += MOVE_AMOUNT;
+      break;
+    case 'ArrowLeft':
+      x -= MOVE_AMOUNT;
+      break;
+    case 'ArrowRight':
+      x += MOVE_AMOUNT;
+      break;
+    default:
+      break;
+  }
+  // Draw
+  ctx.lineTo(x, y);
+  ctx.stroke();
 }
 // Write handler for keys
 function handleKey(e) {
   if (e.key.includes('Arrow')) {
     e.preventDefault();
-    draw({key:e.key})
-    
+    draw({ key: e.key });
+
     console.log(`Handling Key`);
   }
 }
 // Clear shake function
-
+function clearCanvas() {
+  canvas.classList.add('shake');
+  ctx.clearRect(0,0,canvasX,canvasY);
+  canvas.addEventListener(
+    'animationend',
+    function () {
+      canvas.classList.remove('shake');
+    },
+    { once: true }
+  );
+}
 // Listen for arrow keys
 window.addEventListener('keydown', handleKey);
+shakeButton.addEventListener('click', clearCanvas);

4ab69b2edf484b5bac2c2ad99a2d710d225d7bf9
Ryan Bennek <rbennek.rb@gmail.com>
2022-09-22 16:49:26 -0500

    Update Tabs.js

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,1 +1,20 @@
 console.log('ya ya wes we get it.. IT WORKS!');
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('[role="tab"');
+const tabPanels = tabs.querySelectorAll('[role="tabpanel"');
+
+function handleTabClick(e) {
+  console.log(e.currentTarget);
+
+  // hide all tab buttons
+  tabPanels.forEach((panel) => {
+    panel.hidden = true;
+  });
+  // mark all tabs as unselected
+  // mark the clicked tab as selected
+  // find the associated tabPanel and show it
+}
+
+tabButtons.forEach((button) =>
+  button.addEventListener('click', handleTabClick)
+);

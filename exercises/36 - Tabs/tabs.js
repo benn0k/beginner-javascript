a5f91e1ca9f93c874b461f980890b90bbc1fd378
Ryan Bennek <rbennek.rb@gmail.com>
2022-10-07 08:40:46 -0500

    Update Rotate Turtle

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,18 +1,34 @@
 console.log('ya ya wes we get it.. IT WORKS!');
 const tabs = document.querySelector('.tabs');
 const tabButtons = tabs.querySelectorAll('[role="tab"');
-const tabPanels = tabs.querySelectorAll('[role="tabpanel"');
+const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"'));
 
 function handleTabClick(e) {
-  console.log(e.currentTarget);
+  // console.log(e.currentTarget);
 
-  // hide all tab buttons
+  // hide all tab panels
   tabPanels.forEach((panel) => {
     panel.hidden = true;
   });
   // mark all tabs as unselected
+  tabButtons.forEach((tab) => {
+    tab.setAttribute('aria-selected', false);
+  });
   // mark the clicked tab as selected
+
+  e.currentTarget.setAttribute('aria-selected', true);
   // find the associated tabPanel and show it
+  const { id } = e.currentTarget;
+
+  const tabPanel = tabPanels.find(
+    (panel) => panel.getAttribute('aria-labelledby') === id
+  );
+  tabPanel.hidden = false;
+
+  // const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
+  // tabPanel.hidden = false;
+
+  // console.log(id);
 }
 
 tabButtons.forEach((button) =>
